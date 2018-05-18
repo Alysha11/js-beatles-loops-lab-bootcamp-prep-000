af38c4820096ec42d3fedca315b99515a02f97b7
Alysha Neeamuth <alyshanee@gmail.com>
2018-05-18 13:34:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments) {
   var artist; 
   var instrument = [];
-  for (i = 0;  i< musicians.length; i ++) {
+  for (var i = 0;  i< musicians.length; i ++) {
   
   artist=musicians[i] + 'plays' + instruments[i];
   instrument [i] = aritist;
