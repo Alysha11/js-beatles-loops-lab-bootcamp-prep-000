ddea0231100248443c1cbc9fd6795e67ba8ba4a2
Alysha Neeamuth <alyshanee@gmail.com>
2018-05-18 13:05:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var artists = [];
+  for (i = 0;  i< musicians.length; i ++) {
+  
+  artists.push(musicians [i] + "plays "+ instruments  [i] );
+}
+return artists;
+
+
+}
